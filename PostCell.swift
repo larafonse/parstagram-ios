60adee775efad187f7e0f21c9c8bfed5dd5f1940
Nicolas Lara Fonseca <53790517+larafonse@users.noreply.github.com>
2022-03-08 23:08:19 -0800

    first commit

---
+++ PostCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostCell.swift
+//  ParstagramiOS
+//
+//  Created by Nicolas Lara on 3/8/22.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
